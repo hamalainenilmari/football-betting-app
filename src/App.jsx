5c0347e6975eb935f5056bee87bdf934062972fe
Ilmari Hämäläinen <ilmari.hamalainen@aalto.fi>
2024-06-14 21:08:25 +0300

    modify front

--- src/App.jsx
+++ src/App.jsx
@@ -1,149 +1,272 @@
-import { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from "react-bootstrap";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { Predictions } from "./pages/Predictions";
-import { Users } from "./pages/Users";
-import Navbar from "./components/Navbar";
-import Login from './components/Login';
+import { useState, useEffect } from 'react'
+import matchService from './services/matches'
+import {login, signup} from './services/userManagement'
+import Notification from './components/Notification'
+import Match from './components/Match'
+import axios from 'axios'
 
-function App() {
-  
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  /*
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedUser');
-    if (loggedUserJSON) {
-      setIsLoggedIn(true);
-    }
-  }, []);
-  */
-
-  // eslint-disable-next-line no-unused-vars
-
+const App = () => {
+  const [matches, setMatches] = useState([])
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [newUserUsername, setNewUserUsername] = useState('')
   const [newUserPassword, setNewUserPassword] = useState('')
   const [user, setUser] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
-
+  const [notification, setNotification] = useState(null)
+    
   let token = null
 
   const setToken = newToken => {
     token = `Bearer ${newToken}`
   }
 
-    useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedUser')
-    if (loggedUserJSON) {
-        const user = JSON.parse(loggedUserJSON)
-        setUser(user)
-        setToken(user.token)
-    }
-    }, [])
-
-  const handleLogout = () => {
-    window.localStorage.removeItem('loggedUser');
-    setIsLoggedIn(false);
-  };
-
-  /*
-  const handleLogin = () => {
-    window.localStorage.setItem('loggedUser');
-    setIsLoggedIn(true);
-  };
-  */
-
+  useEffect(() => {
+    matchService.getAll().then(matches =>
+      setMatches( matches )
+    )
+  }, [])
 
   useEffect(() => {
-  const loggedUserJSON = window.localStorage.getItem('loggedUser')
-  if (loggedUserJSON) {
+    const loggedUserJSON = window.localStorage.getItem('loggedUser')
+    if (loggedUserJSON) {
       const user = JSON.parse(loggedUserJSON)
       setUser(user)
       setToken(user.token)
-  }
+    }
   }, [])
 
+  const inputContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+  };
+  
+  const labelStyle = {
+    marginBottom: '5px',
+    fontSize: '14px',
+  };
+  
+  const inputStyle = {
+    padding: '8px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    transition: 'border-color 0.3s ease',
+    fontSize: '16px',
+  };
+  
+  const notificationStyle = {
+    transition: 'opacity 0.3s ease',
+  };
+
   const handleLogin = async (event) => {
-      event.preventDefault()
+    event.preventDefault()
 
-      try {
-          const user = await login({
-          username, password
-          })
-          window.localStorage.setItem(
-          'loggedUser', JSON.stringify(user)
-          )
-          
-          setToken(user.token)
-          setUser(user)
-          setUsername('')
-          setPassword('')
-          setIsLoggedIn(true)
+    try {
+      const user = await login({
+        username, password
+      })
+      window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+      )
+      setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Incorrect username or password')
+      setTimeout( () => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+    console.log('logging in with', username, password)
+  }
 
-      } catch (exception) {
-          setErrorMessage('Incorrect username or password')
-          setTimeout( () => {
-          setErrorMessage(null)
-          }, 5000)
-      }
-      console.log('logging in with', username, password)
-      }
+  const logout = () => {
+    window.localStorage.removeItem('loggedUser')
+    setUser(null)
+  }
 
+  const buttonStyle = {
+    padding: '8px 16px',
+    background: '#007bff',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '4px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s ease',
+    fontSize: '16px',
+  };
+  /*
+  buttonStyle:hover = {
+    background: '#0056b3',
+  };
+  */
 
-  if (!user) {
+  const loginForm = () => {
     return (
       <form onSubmit={handleLogin}>
-      <h1>EM 2024 veikkausskaba</h1>
-      <div>
+        <Notification message={errorMessage} style={notificationStyle} />
+        <div style={{ marginBottom: '10px' }}>
+        <div style={inputContainerStyle}>
           <h2>Kirjaudu sisään</h2>
-          <Notification message={errorMessage} />
-          käyttäjätunnus 
-          <input 
-            style={{ marginLeft: '5px' }}
+          <label style={labelStyle}>käyttäjätunnus</label>
+          <input
             data-testid='username'
             type="text"
             value={username}
             name="Username"
-            onChange={({ target }) => setUsername(target.value)} 
+            onChange={({ target }) => setUsername(target.value)}
+            style={inputStyle}
           />
-      </div>
-      <div>
-          salasana
+          </div>
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          <div style={inputContainerStyle}>
+            <label style={labelStyle}>salasana</label>
           <input
-          style={{ marginLeft: '5px' }}
-          data-testid='password'
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
+            data-testid='password'
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+            style={inputStyle}
           />
-      </div>
-      <button type="submit">kirjaudu</button>
+        </div>
+        </div>
+        <button style={buttonStyle} type="submit">kirjaudu</button>
       </form>
     )
-  } else {
+  }
 
+  const handleSignup = async (event) => {
+    event.preventDefault()
+
+    try {
+        const user = await signup({
+        username: newUserUsername,
+        password: newUserPassword
+        })
+        window.localStorage.setItem(
+        'loggedUser', JSON.stringify(user)
+        )
+        setToken(user.token)
+        setUser(user)
+        setNewUserPassword('')
+        setNewUserUsername('')
+    } catch (exception) {
+        setErrorMessage('Error signing up')
+        setTimeout( () => {
+        setErrorMessage(null)
+        }, 5000)
+    }
+    console.log('logging in with', username, password)
+}
+
+
+const signupForm = () => {
     return (
-      <>
-      
-      <Router>
-      <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
-      <Container className="mb-4" style={{ marginTop: '80px' }}>
-        <Routes>
-          <Route path="/home" element={<Home />} />
-          <Route path="/Predictions" element={<Predictions />} />
-          <Route path="/Users" element={<Users />} />
-        </Routes>
-      </Container>
-    </Router>
-    </>
-  );
+        <form onSubmit={handleSignup}>
+          <Notification message={errorMessage} style={notificationStyle} />
+          <div style={{ marginBottom: '10px' }}>
+          <div style={inputContainerStyle}>
+            <label style={labelStyle}>käyttäjätunnus</label>
+            <input
+              data-testid='newUserUsername'
+              type="text"
+              value={newUserUsername}
+              name="Username"
+              onChange={({ target }) => setNewUserUsername(target.value)}
+              style={inputStyle}
+            />
+          </div>
+          </div>
+          <div style={{ marginBottom: '10px' }}>
+          <div style={inputContainerStyle}>
+            <label style={labelStyle}>salasana</label>
+              <input
+              data-testid='newUserPassword'
+              type="password"
+              value={newUserPassword}
+              name="Password"
+              onChange={({ target }) => setNewUserPassword(target.value)}
+              style={inputStyle}
+              />
+          </div>
+          </div>
+          <button style={buttonStyle} type="submit">Luo tunnus</button>
+        </form>
+    )
+  }
+
+  const makePrediction = async ( match, homeGoalsPrediction, awayGoalsPrediction ) => {
+    try {
+      console.log("goals: " + homeGoalsPrediction, awayGoalsPrediction)
+      const prediction = {
+        username: user.username,
+        matchId: match.id,
+        home: match.home,
+        away: match.away,
+        homeGoals: homeGoalsPrediction,
+        awayGoals: awayGoalsPrediction,
+        winner: homeGoalsPrediction > awayGoalsPrediction ? match.home : awayGoalsPrediction > homeGoalsPrediction ? match.away : 'draw',
+      }
+      const response = await axios.post('http://localhost:3000/api/predictions', prediction);
+      console.log(response)
+      if (response.status === 201) {
+        console.log("insertion succesful");
+        setNotification(`Veikkaus ${match.home} ${homeGoalsPrediction} - ${awayGoalsPrediction} ${match.away} isketty sisään (kusee varmaan)`)
+        setTimeout( () => {
+          setNotification(null)
+          matchService.getAll().then(matches =>
+            setMatches(matches)
+          );
+          }, 5000)
+      } else {
+          console.log("here")
+          setNotification("Veikkauksen tallentaminen ei onnistunut. Johtuu varmaan lukaksesta :/")
+          setTimeout( () => {
+            setNotification(null)
+            }, 5000)
+      }
+    } catch (err) {
+      setNotification("Veikkauksen tallentaminen ei onnistunut. Johtuu varmaan lukaksesta :/")
+          setTimeout( () => {
+            setNotification(null)
+            }, 5000)
+    }
+  }
+
+  if (user) {
+    return(
+      <div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <p>{user.username}</p>
+        <p><button onClick={logout}>Kirjaudu ulos</button></p>
+      </div>
+        <Notification message={notification}/>
+        <Notification message={errorMessage}/>
+        {matches.map(match =>
+          <Match key={match.id}
+            match={match}
+            makePrediction={makePrediction}
+            user={user}
+          />
+        )}
+        <p>päivitä sivu kun oot veikannu nii ne paskat näkyy</p>
+        <p>loput äpist tulee myöhemmi. jos joku leikkii ja ettii bugei (niit on) nii väärennän sun veikkaukset </p>
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>Bettiäppi</h2>
+        {loginForm()}
+        <h2>Luo uusi käyttäjä</h2>
+        {signupForm()}
+      </div>
+    ) 
+  }
 }
 
-}
-
-export default App;
+export default App
---
+++ src/components/Match.jsx
@@ -0,0 +1,181 @@
+import { useState, useEffect } from 'react'
+import PropTypes from 'prop-types';
+import axios from 'axios';
+
+const Match = ({ match, makePrediction, user }) => {
+
+  const [homeGoalsPrediction, setHomeGoalsPrediction] = useState(0)
+  const [awayGoalsPrediction, setAwayGoalsPrediction] = useState(0)
+  const [homeGoalsPredictionToShow, setHomeGoalsPredictionToShow] = useState(0)
+  const [awayGoalsPredictionToShow, setAwayGoalsPredictionToShow] = useState(0)
+  const [predictionExists, setPredictionExists] = useState(false)
+  const [matchStarted, setMatchStarted] = useState(false)
+  const [matchTime, setMatchTime] = useState('')
+
+  const timeStyle = {
+    fontSize: '14px',
+    color: '#333',
+    marginRight: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%', // Make sure the time spans the entire width
+    marginBottom: '10px' // Add margin bottom for spacing
+  };
+
+
+  const matchStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+  };
+
+  const teamStyle = {
+    display: 'flex',
+    alignItems: 'center',
+  };
+
+  const goalsInputStyle = {
+    marginLeft: 10,
+  };
+
+  const handleHomeGoalsPrediction = (event) => {
+    setHomeGoalsPrediction(Number(event.target.value));
+  };
+
+  const handleAwayGoalsChange = (event) => {
+    setAwayGoalsPrediction(Number(event.target.value));
+  };
+
+  const handleMakePrediction = () => {
+    makePrediction(match, homeGoalsPrediction, awayGoalsPrediction);
+  };
+
+
+  const userHasAlreadyMadePredicion = async () => {
+    try {
+        const response = await axios.get(`http://localhost:3000/api/predictions/username/${user.username}`);
+        if (response.data) {
+            for (const matchToSearch of response.data) {
+            if (matchToSearch.matchId === match.id) {
+                console.log("here") 
+                setHomeGoalsPredictionToShow(matchToSearch.homeGoals);
+                setAwayGoalsPredictionToShow(matchToSearch.awayGoals);
+                setPredictionExists(true);
+                return true; // Return true if match is found
+            }
+            }
+        }
+        return false; // Return false if match is not found in predictions
+        } catch (err) {
+        console.log("Error fetching predictions:", err);
+        }
+    }
+
+    useEffect(() => {
+        const hasMatchStarted = new Date(match.date) <= new Date();
+        setMatchStarted(hasMatchStarted);
+        userHasAlreadyMadePredicion();
+        setMatchTime(formatDate(match.date))
+      }, []);
+
+      const dateStyle = {
+        position: 'absolute', // Position the date absolutely within the parent container
+        top: 0, // Position at the top
+        left: '50%', // Move to the middle horizontally
+        transform: 'translateX(-50%)', // Center horizontally
+        backgroundColor: 'rgba(255,255,255,0.8)', // Semi-transparent background
+        padding: '5px', // Add padding for better readability
+      };
+ 
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const options = {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+          hour: 'numeric',
+          minute: 'numeric',
+          second: 'numeric',
+          hour12: false,
+          timeZone: 'Europe/Helsinki' // Set timezone to Helsinki
+        };
+        return date.toLocaleString('en-US', options);
+      };
+
+      const isMatchInFuture = () => {
+        const today = new Date();
+        const matchDate = new Date(match.date);
+        const timeDiff = matchDate.getTime() - today.getTime();
+        const daysDiff = timeDiff / (1000 * 3600 * 24);
+        return daysDiff >= 1.5;
+      };
+    
+      // If match is two or more days later than today, don't render
+      if (isMatchInFuture()) {
+        return null;
+      }
+
+  return (
+    <div>
+        <span style={timeStyle}> {matchTime} </span>
+    <div className='match' style={matchStyle} display={'flex'}>
+      <div style={teamStyle}>
+        <span>{match.home}</span>
+        <img src={match.homeLogo} style={{ width: '30px', height: '30px' }} />
+        {(!predictionExists && !matchStarted) ? (
+          <input 
+            type="number" 
+            value={homeGoalsPrediction} 
+            onChange={handleHomeGoalsPrediction} 
+            min="0"
+            style={goalsInputStyle}
+          />
+        ) : ( <span>{homeGoalsPredictionToShow}</span>) }
+        </div>
+        <span> - </span>
+        <div style={teamStyle}>
+        {(!predictionExists && !matchStarted) ? (
+          <input 
+            type="number" 
+            value={awayGoalsPrediction} 
+            onChange={handleAwayGoalsChange} 
+            min="0"
+            style={goalsInputStyle}
+          /> ) : ( <span>{awayGoalsPredictionToShow}</span>) }
+          <img src={match.awayLogo} style={{ width: '30px', height: '30px' }} />
+          <span>{match.away}</span>
+          {(!predictionExists && !matchStarted) && (<button onClick={handleMakePrediction}>Make Prediction</button>)}
+      </div>
+    </div>
+    </div>
+  )
+}
+
+Match.propTypes = {
+    match: PropTypes.shape({
+      date: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+      home: PropTypes.string.isRequired,
+      homeLogo: PropTypes.string.isRequired,    
+      away: PropTypes.string.isRequired,
+      awayLogo: PropTypes.string.isRequired,
+      homeGoals: PropTypes.number,
+      awayGoals: PropTypes.number,
+      winner: PropTypes.string
+    }).isRequired,
+    makePrediction: PropTypes.func.isRequired,
+    user: PropTypes.shape({
+        token: PropTypes.string.isRequired,
+        username: PropTypes.string.isRequired,
+    })
+};
+
+
+export default Match
